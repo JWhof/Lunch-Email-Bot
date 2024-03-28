2a09d99972a3ec956baaf8dd679707e466806c65
JWhof <romanczyy@gmail.com>
2024-03-28 16:08:30 +0100

    OOP update, cleaned up code

--- main/main.py
+++ main/main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import dateutil.parser
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
@@ -8,85 +8,72 @@ import re
 use_override_date = True
 override_date = "2024-03-28"
 
+class LunchBot:
+    def __init__(self):
+        self.events = None
+        self.event_day = None
+        self.lunch_items_list = []
+        self.lunch_items_dict = {}
 
-
-def main():
-    global events
-
-    SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-    creds = Credentials.from_authorized_user_file("secret/token.json", SCOPES)
-    service = build("calendar", "v3", credentials=creds)
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    events_result = (
-        service.events()
-        .list(
-            calendarId="nij10q76sud67hkgebrhs7nboil1gsld@import.calendar.google.com",
-            timeMin=now,
-            maxResults=10,
-            singleEvents=True,
-            orderBy="startTime",
+    def setup(self):
+        SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+        creds = Credentials.from_authorized_user_file("secret/token.json", SCOPES)
+        service = build("calendar", "v3", credentials=creds)
+        now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        events_result = (
+            service.events()
+            .list(
+                calendarId="nij10q76sud67hkgebrhs7nboil1gsld@import.calendar.google.com",
+                timeMin=now,
+                maxResults=10,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
         )
-        .execute()
-    )
-    events = events_result.get("items", [])
+        self.events = events_result.get("items", [])
 
-
-
-
-def retrieve_correct_day_info() -> None:
-    """Retrieves the current day/override day's lunch menu and stores it in lunch_items list. 
+    def retrieve_day_info(self) -> None:
+        """Retrieves the current day/override day's lunch menu and stores it in lunch_items list. 
     Throws NoLunchFoundError if there is no lunch available on that day (likely a weekend/holiday)."""
+        if use_override_date:
+            for event in self.events:
+                if str(event["start"].get("dateTime", event["start"].get("date"))) == override_date and "ES" not in event["summary"]:
+                    self.event_day = str(event["start"].get("dateTime", event["start"].get("date")))
+                    self.lunch_items_list = [line for line in event["description"].split("\n") if line != ""]
+        else:
+            for event in self.events:
+                if str(event["start"].get("dateTime", event["start"].get("date"))) == str(datetime.utcnow().isoformat(timespec="hours").split("T")[0]) and "ES" not in event["summary"]:
+                    self.event_day = str(event["start"].get("dateTime", event["start"].get("date")))
+                    self.lunch_items_list = [line for line in event["description"].split("\n") if line != ""]
+        self.lunch_items_list = [lunch_item.strip() for lunch_item in self.lunch_items_list]
 
-    global lunch_items_list
-
-    lunch_items_list = []
-
-    if use_override_date:
-        for event in events:
-            if str(event["start"].get("dateTime", event["start"].get("date"))) == override_date and "ES" not in event["summary"]:
-                lunch_items_list = [line for line in event["description"].split("\n") if line != ""]
-    else:
-        for event in events:
-            if str(event["start"].get("dateTime", event["start"].get("date"))) == str(datetime.datetime.utcnow().isoformat(timespec="hours").split("T")[0]) and "ES" not in event["summary"]:
-                lunch_items_list = [line for line in event["description"].split("\n") if line != ""]
-    lunch_items_list = [lunch_item.strip() for lunch_item in lunch_items_list]
-
-    print(lunch_items_list)
-
-    if lunch_items_list == []:
-        raise no_lunch_found.NoLunchFoundError("No lunch could be found for this date.")
-        
-
-def create_lunch_items_dict(day) -> None:
-    """Separates the items from lunch_items_list into keys and values in lunch_items dict. 
-    Includes a try except if vegetarian hot lunch is not available."""
-
-    global lunch_items_dict
-    lunch_items_dict = {}
-
-    soup_regex_pattern = r'\b[sS][oO][uU][pP][sS]\s*:?\s*'
-    snack_regex_pattern = r'\b[hH][oO][tT]\s+[sS][nN][aA][cC][kK][sS]?\s*:?\s*'
-    lunch_regex_pattern = r'\b[hH][oO][tT]\s+[lL][uU][nN][cC][hH]\s*:?\s*'
-
-    lunch_items_dict["Soups"] = re.sub(soup_regex_pattern, '', lunch_items_list[0], count=1)
-    lunch_items_dict["Snack"] = re.sub(snack_regex_pattern, '', lunch_items_list[1], count=1)
-    lunch_items_dict["Lunch"] = re.sub(lunch_regex_pattern, '', lunch_items_list[2], count=1)
+        if self.lunch_items_list == []:
+            raise no_lunch_found.NoLunchFoundError("No lunch could be found for this date.")
     
-    if is_day_monday(day):
-        print("day is monday")
-    else:
-        print("day is not monday")
+    def init_menu_dict(self) -> None:
+        """Separates the items from lunch_items_list into keys and values in lunch_items dict. 
+        Includes a try except if vegetarian hot lunch is not available."""
+        soup_regex_pattern = r'\b[sS][oO][uU][pP][sS]\s*:?\s*'
+        snack_regex_pattern = r'\b[hH][oO][tT]\s+[sS][nN][aA][cC][kK][sS]?\s*:?\s*'
+        lunch_regex_pattern = r'\b[hH][oO][tT]\s+[lL][uU][nN][cC][hH]\s*:?\s*'
 
-    print(lunch_items_dict)
+        self.lunch_items_dict["Soups"] = re.sub(soup_regex_pattern, '', self.lunch_items_list[0], count=1)
+        self.lunch_items_dict["Snack"] = re.sub(snack_regex_pattern, '', self.lunch_items_list[1], count=1)
+        self.lunch_items_dict["Lunch"] = re.sub(lunch_regex_pattern, '', self.lunch_items_list[2], count=1)
+
+        if not self.is_day_monday():
+            veg_lunch_regex_pattern = r'\b[hH][oO][tT]\s+[lL][uU][nN][cC][hH]\s*\([vV]\)\s*:?\s*'
+            self.lunch_items_dict["Veg Lunch"] = re.sub(veg_lunch_regex_pattern, '', self.lunch_items_list[3], count=1)
     
+    def is_day_monday(self) -> bool:
+        date_obj = datetime.strptime(self.event_day, "%Y-%m-%d")
+        self.weekday = date_obj.weekday()
+        return self.weekday == 0
 
-def is_day_monday(day) -> bool:
-    event_day = dateutil.parser.parse(day)
-    day_of_week = event_day.weekday()
-    if day_of_week == 0:
-        return True
-    return False
+bot = LunchBot()
 
-
-if __name__ == "__main__":
-    main()
+bot.setup()
+bot.retrieve_day_info()
+bot.init_menu_dict()
+print(bot.lunch_items_dict)
